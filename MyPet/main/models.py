814193be3474fde5b7a61ee5549f68b17b66282c
yohan11 <yipco77@naver.com>
2023-05-09 18:40:47 +0900

    feat(#1): 로그인 페이지 퍼블리싱 구현

--- MyPet/main/models.py
+++ MyPet/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nickname = models.CharField(max_length=100, blank=False, null=False)
+    password = models.CharField(max_length=100, blank=False, null=False)
+    
+    def __str__(self):
+        return self.nickname
+    
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey("User", related_name="user", on_delete=models.CASCADE, db_column="user_id")
+    img = models.FileField(null=False)
+    description = models.TextField(null=False)
+
+    def __str__(self):
+        return self.id
+
+
--- MyPet/main/views.py
+++ MyPet/main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request,'main/index.html')
+    return render(request,'index.html')
+
+def feed(request):
+    # 모든 Post를 가져와 postlist에 저장합니다
+    postlist = Post.objects.all()
+    # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다 
+    return render(request, 'feed.html', {'postlist':postlist})
+
