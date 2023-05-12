1016d5738193acc6857e9fd65219954605844eaf
yohan11 <yipco77@naver.com>
2023-05-12 18:45:23 +0900

    feat(#3): 피드 페이지 퍼블리싱 구현

--- MyPet/MyPet/settings.py
+++ MyPet/MyPet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,4 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#프로젝트 진행하면서 바꾼 부분
 LOGIN_REDIRECT_URL = '/main/feed'
+
+MEDIA_URL = '/media/'		
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- MyPet/main/models.py
+++ MyPet/main/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE,db_column="user_id")
-    img = models.FileField(null=False)
+    img = models.ImageField(null=False, upload_to="post/%Y%m%d")
     description = models.TextField(null=False)
 
     def __str__(self):
