6d6d4ad01f5ec099fa2d2f2e53045cfb2ab20da0
yohan11 <yipco77@naver.com>
2023-05-17 00:02:59 +0900

    추가기능 > 회원가입 기능 구현

--- MyPet/MyPet/settings.py
+++ MyPet/MyPet/settings.py
@@ -86,21 +86,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+# 테스트 편의상 validation 제거
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
 # Internationalization
--- MyPet/common/urls.py
+++ MyPet/common/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'common'
 
 urlpatterns = [
     path('/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
